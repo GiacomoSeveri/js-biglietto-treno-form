96bda350593ee8b58b093d22b1c2522833c6073e
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-06 16:17:46 +0100

    added html things

--- js/script.js
+++ js/script.js
@@ -9,42 +9,18 @@
 
 
 // Prendo l'elemento dal DOM
-const targetElement = document.getElementById('ticket');
+// const targetElement = document.getElementById('ticket');
+const km = document.getElementById('km');
+const ageUser = document.getElementById('age');
+const calc = document.getElementById('calc');
+const reset = document.getElementById('reset');
 // console.log(targetElement);
 
 // Creo la costante del prezzo
 const price = 0.21;
 
-// Chiedere i chilometri
-const kilometres = prompt('Quanti chilometri vuoi fare?' , '50');
-// console.log(kilometres);
+calc.addEventListener('click', function () {
+    const allPrice = km * price;
+    console.log(allPrice);
+});
 
-// Chiedere l'età
-const age = prompt('Quanti anni hai?' , '18');
-// console.log(age);
-
-// 5 Calcolo il prezzo
-const totalPrice = price * kilometres;
-
-let newPrice = 0;
-
-
-if (age < 18) {
-    const discount = totalPrice * 0.20;
-    // console.log(discount);
-    newPrice = totalPrice - discount;
-    // console.log(newPrice);
-}
-else if (age > 65) {
-    const discount = totalPrice * 0.40;
-    // console.log(discount);
-   newPrice = totalPrice - discount;
-} 
-else {
-    newPrice = totalPrice;
-}
-
-newPrice = newPrice.toFixed(2);
-
-// Inseriamo il isultato nell'HTML
-targetElement.innerText = `Hai deciso di fare ${kilometres} KM, Hai ${age} anni: Il tuo biglietto costa ${newPrice} €.`;
