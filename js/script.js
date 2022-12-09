b59c0c60b7b7d937faf4c9b7d63e5d68795fd9ee
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-09 12:45:17 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -4,21 +4,58 @@
 // 2 Lego tutto sull'event listener del bottone di conferma
 // 3 Recupero i valori del form
 // 4 Calcolo il prezzo
-// 5 Mostro il biglietto con i giusti valori
+// 5 Calcolo lo sconto
+// 6 Mostro il biglietto con i giusti valori
 
 
 // 1
-const nameValue = document.getElementById('name');
-const kmValue = document.getElementById('km');
-const ageValue = document.getElementById('age');
-const confirmValue = document.getElementById('c-button');
-const resetValue = document.getElementById('r-button');
+const nameField = document.getElementById('name');
+const kmField = document.getElementById('km');
+const ageField = document.getElementById('age');
+const confirmField = document.getElementById('c-button');
+const resetField = document.getElementById('r-button');
 
-const tiketSValue = document.getElementById('tiket');
-const tiketNameValue = document.getElementById('p-name');
-const rateValue = document.getElementById('rate');
-const coachValue = document.getElementById('coach');
-const pnrValue = document.getElementById('pnr');
-const priceValue = document.getElementById('price');
+const tiketSField = document.getElementById('tiket');
+const tiketNameField = document.getElementById('p-name');
+const rateField = document.getElementById('rate');
+const coachField = document.getElementById('coach');
+const pnrField = document.getElementById('pnr');
+const priceField = document.getElementById('price');
 
 // 2
+confirmValue.addEventListener('click', function(){
+
+    // 3
+    const nameValue = nameField.value;
+    const kmValue = parseInt(kmField.value.trim());
+    const ageValue = ageField.value;
+
+    if(!nameValue || isNaN(kmValue) || kmValue < 10){
+        alert('Hai inserito valori non validi');
+        return;
+    };
+
+    // 4
+    let rateName = 'Biglietto standard'
+    let price = 0.21 * km;
+
+    // 5
+    if(ageValue === 'min'){
+        price *= 0.8;
+        rateName = 'Biglietto ridotto minorenni';
+    } else is (ageValue === over){
+        price *= 0.6;
+        rateName = 'Biglietto ridotto over 65';
+    };
+
+    price = price.toFixed(2);
+
+    // 6 
+    tiketNameField.innerText = nameValue;
+    rateField.innerText = rateName;
+    coachField.innerText = '9';
+    pnrField.innerText = '220703';
+    priceField.innerText = price;
+
+    tiketSField.clssList.remove('d-none');
+})
