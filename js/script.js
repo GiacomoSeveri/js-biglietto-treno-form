e5e94a440fdc21a5d50cbdc62d1801c28b94b061
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-09 12:16:42 +0100

    take elemnt

--- js/script.js
+++ js/script.js
@@ -1,26 +1,16 @@
 // console.log('js ok')
 
 // 1 Prendo l'elemento dal DOM
-// 2 Creo la costante del prezzo
+// 2 Lego tutto sull'event listener del bottone di conferma
 // 3 Chiedo il numero di chilometri all'utente
 // 4 Chiedo l'età all'utente
 // 5 Calcolo il prezzo
 // 6 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
 
-
-// Prendo l'elemento dal DOM
-// const targetElement = document.getElementById('ticket');
-const km = document.getElementById('km');
-const ageUser = document.getElementById('age');
-const calc = document.getElementById('calc');
-const reset = document.getElementById('reset');
-// console.log(targetElement);
-
-// Creo la costante del prezzo
-const price = 0.21;
-
-calc.addEventListener('click', function () {
-    const allPrice = km * price;
-    console.log(allPrice);
-});
+// 1
+const nameValue = document.getElementById('name')
+const kmValue = document.getElementById('km')
+const ageValue = document.getElementById('age')
+const confirmValue = document.getElementById('c-button')
+const resetValue = document.getElementById('r-button')
 
