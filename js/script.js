7430ffaa927e956e2007ff7d5f9934a74c6483d9
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-09 12:24:01 +0100

    added element

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@
 
 // 1 Prendo l'elemento dal DOM
 // 2 Lego tutto sull'event listener del bottone di conferma
-// 3 Chiedo il numero di chilometri all'utente
-// 4 Chiedo l'età all'utente
-// 5 Calcolo il prezzo
-// 6 Inserisco nell'HTML il prezzo calcolato con eventuali sconti
+// 3 Recupero i valori del form
+// 4 Calcolo il prezzo
+// 5 Mostro il biglietto con i giusti valori
+
 
 // 1
-const nameValue = document.getElementById('name')
-const kmValue = document.getElementById('km')
-const ageValue = document.getElementById('age')
-const confirmValue = document.getElementById('c-button')
-const resetValue = document.getElementById('r-button')
+const nameValue = document.getElementById('name');
+const kmValue = document.getElementById('km');
+const ageValue = document.getElementById('age');
+const confirmValue = document.getElementById('c-button');
+const resetValue = document.getElementById('r-button');
 
+const tiketSValue = document.getElementById('tiket');
+const tiketNameValue = document.getElementById('p-name');
+const rateValue = document.getElementById('rate');
+const coachValue = document.getElementById('coach');
+const pnrValue = document.getElementById('pnr');
+const priceValue = document.getElementById('price');
+
+// 2
